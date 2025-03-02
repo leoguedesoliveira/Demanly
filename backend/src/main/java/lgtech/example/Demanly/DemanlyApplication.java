f47c0f76563c9abc73d894e7a82dd3409b13ca19
leoguedesoliveira <leonardoguedes.dasilvaguedesol@gmail.com>
2025-03-02 14:49:52 -0300

    Criando projeto backend

---
+++ backend/src/main/java/lgtech/example/Demanly/DemanlyApplication.java
@@ -0,0 +1,13 @@
+package lgtech.example.Demanly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemanlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemanlyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/lgtech/example/Demanly/DemanlyApplicationTests.java
@@ -0,0 +1,13 @@
+package lgtech.example.Demanly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemanlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
